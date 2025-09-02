e63851da293e35c1b42a0992637afb9c1288aff3
VL4RR <146389401+VL4RR@users.noreply.github.com>
2025-09-02 13:22:44 +0100

    Update Autopilot.ps1

--- Autopilot.ps1
+++ Autopilot.ps1
@@ -1,7 +1,6 @@
 ﻿## This script will check if the device is in Autopilot. If it is, it will print the group tag of the device.
 ## It will then proceed to remove the Intune record if required, then install Windows & drivers
 
-Write-host "Chris"
 
 $winVer = "Windows 11 23H2 x64"
 
